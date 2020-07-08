73b1eeb724bc02ddeb72b45ae584b2de374810aa
David Portilla <dportilla@adkgroup.com>
2020-07-08 18:15:05 -0500

    remove login with facebook option

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ const Login = (props) => (
   <nav className="login" >
     <h2>Inventory Login</h2>
     <button className="github" onClick={ () => props.authenticate('Github') }> Log in with Github</button>
-    <button className="facebook" onClick={ () => props.authenticate('Facebook') }> Log in with Facebook</button>
+    {/* <button className="facebook" onClick={ () => props.authenticate('Facebook') }> Log in with Facebook</button> */}
     <button className="twitter" onClick={ () => props.authenticate('Twitter') }> Log in with Twitter</button>
   </nav>
 )
